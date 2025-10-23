f587d3b4a1996918c6e9763a22329baa323f17c5
BenyiCordero <benyi.uriel2006@gmail.com>
2025-10-23 13:58:01 -0600

    Se crea el repositorio del Token

---
+++ src/main/java/com/bcss/checador/repository/TokenRepository.java
@@ -0,0 +1,13 @@
+package com.bcss.checador.repository;
+
+import com.bcss.checador.domain.Token;
+import com.bcss.checador.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<Token,Long> {
+    Optional<Token> findByToken(String token);
+    List<Token> findByUsuario(Usuario usuario);
+}
