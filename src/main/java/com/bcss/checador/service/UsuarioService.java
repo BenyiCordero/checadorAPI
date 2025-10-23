1d90757fe7b854101776e1c50729c48549b6ab9a
BenyiCordero <benyi.uriel2006@gmail.com>
2025-10-23 14:21:22 -0600

    Se agrega el usuario service y sus metodos

--- src/main/java/com/bcss/checador/service/UsuarioService.java
+++ src/main/java/com/bcss/checador/service/UsuarioService.java
@@ -1,4 +1,11 @@
 package com.bcss.checador.service;
 
+import com.bcss.checador.domain.Usuario;
+
+import java.util.Optional;
+
 public interface UsuarioService {
+    Usuario saveIfNotExist(Usuario usuario);
+    Optional<Usuario> findByEmail(String email);
+    Optional<Usuario> findById(Integer id);
 }
--- src/main/java/com/bcss/checador/service/UsuarioServiceImpl.java
+++ src/main/java/com/bcss/checador/service/UsuarioServiceImpl.java
@@ -1,4 +1,40 @@
 package com.bcss.checador.service;
 
-public class UsuarioServiceImpl {
+import com.bcss.checador.domain.Usuario;
+import com.bcss.checador.exception.EmailRepetidoException;
+import com.bcss.checador.exception.UsuarioNoEncontradoException;
+import com.bcss.checador.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioServiceImpl(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public Usuario saveIfNotExist(Usuario usuario) {
+        if(findByEmail(usuario.getEmail()).isPresent()) throw new EmailRepetidoException();
+        return usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public Optional<Usuario> findByEmail(String email) {
+        Optional<Usuario> usuario = usuarioRepository.findByEmail(email);
+        if(usuario.isPresent()) return usuario;
+        else throw new UsuarioNoEncontradoException();
+    }
+
+    @Override
+    public Optional<Usuario> findById(Integer id) {
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+        if(usuario.isPresent()) return usuario;
+        else throw new UsuarioNoEncontradoException();
+    }
+
 }
