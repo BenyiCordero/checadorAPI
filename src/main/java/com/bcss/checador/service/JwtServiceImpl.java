10c54d41718babc667fdf3439fc6bbf38e7599cf
BenyiCordero <benyi.uriel2006@gmail.com>
2025-10-23 13:59:57 -0600

    Se aniade la anotacion al servicio

--- src/main/java/com/bcss/checador/service/JwtServiceImpl.java
+++ src/main/java/com/bcss/checador/service/JwtServiceImpl.java
@@ -2,15 +2,16 @@ package com.bcss.checador.service;
 
 import com.bcss.checador.domain.Usuario;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.Map;
 
+@Service
 public class JwtServiceImpl implements JwtService {
 
     @Value("${application.security.jwt.secret-key}")
